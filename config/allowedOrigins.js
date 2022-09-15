0fd5d4ec16703f0778dffa347b407a43434a91f7
Alan Chavez <alanchavez1@gmail.com>
2022-09-14 18:38:26 -0600

    adding website to allowed origings

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,7 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
+    'https://alanchavezt.com',
+    'https://www.alanchavezt.com',
     'https://glimmer-abstracted-jump.glitch.me',
     'http://127.0.0.1:3000',
     'http://localhost:4000',
