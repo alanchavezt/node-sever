7b3aaa4b33307416f77e7a5fd37eb6ad07419324
Alan Chavez <alanchavez1@gmail.com>
2022-09-14 18:24:07 -0600

    adding origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
+    'https://glimmer-abstracted-jump.glitch.me',
     'http://127.0.0.1:3000',
     'http://localhost:4000',
     'http://localhost:3000'
