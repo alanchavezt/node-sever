137f6f11fac5c8639959bf7b65cb88a4cf161406
Alan Chavez <alanchavez1@gmail.com>
2022-10-27 10:42:16 -0600

    adding status code to response and cleanup

--- routes/java-api/signupRoutes.js
+++ routes/java-api/signupRoutes.js
@@ -5,15 +5,11 @@ const axios = require("axios");
 routes.post('/', async (req, res, next) => {
     try {
         const user = req.body;
-        // const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        // user.password = hashedPassword;
-
         const response = await axios.post(`${process.env.API_URL}/API/signup`, user);
 
-        // res.status(200);
         res.status(201).json(response.data);
     } catch (error)  {
-        res.json("Error occurred!" + error);
+        res.status(error.response.status).json(error);
     }
 });
 
