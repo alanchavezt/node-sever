dd8a47748f62928b3fa11a425ef92b7de2fdd0a7
Alan Chavez <alanchavez1@gmail.com>
2022-11-03 18:46:04 -0600

    adding validation to auth route

--- routes/java-api/signupRoutes.js
+++ routes/java-api/signupRoutes.js
@@ -3,8 +3,13 @@ const routes = express.Router();
 const axios = require("axios");
 
 routes.post('/', async (req, res, next) => {
+    const user = req.body;
+    const {firstName, lastName, email,  password, confirmPassword} = req.body;
+    if (!firstName || !lastName || !email || !password || !confirmPassword) {
+        return res.status(400).json({ 'message': 'First Name, Last Name, Email and Password are required.' });
+    }
+
     try {
-        const user = req.body;
         const response = await axios.post(`${process.env.API_URL}/API/signup`, user);
 
         res.status(201).json(response.data);
