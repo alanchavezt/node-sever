7b949b984b11448cf6eea97a0fcc420264d11635
Alan Chavez <a.chavez.1@elsevier.com>
2025-07-23 19:29:16 -0600

    update authRoutes for java api resources

--- routes/java-api/authRoutes.js
+++ routes/java-api/authRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 const axios = require("axios");
 
-routes.post('/', async (req, res) => {
+routes.post("/", async (req, res) => {
     const { email, password } = req.body;
+    const usernameOrEmail = email || req.body.usernameOrEmail;
 
     if (!email || !password) {
         return res.status(400).json({
@@ -13,18 +14,22 @@ routes.post('/', async (req, res) => {
     }
 
     try {
-        const response = await axios.post(`${process.env.API_URL}/api/auth`, {email, password});
+        const response = await axios.post(`${process.env.API_URL}/api/auth`, {
+            usernameOrEmail: usernameOrEmail,
+            password: password
+        });
         const accessToken = response.data.accessToken;
         const user = response.data.user;
         const roles = response.data.roles;
 
-        return res.json({accessToken, user, roles});
+        return res.json({ accessToken, user, roles });
     } catch (error) {
         res.status(401).json({
             error: error,
             message: "Username or Password is Wrong."
         });
-    };
+    }
+    ;
 });
 
 module.exports = routes;
