c4261313041f54c0650181fc6e4277f8ed764f98
Alan Chavez <a.chavez.1@elsevier.com>
2025-11-07 18:22:19 -0700

    giving better feedback when returning 401 and 403

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,11 +1,19 @@
 const verifyRoles = (...allowedRoles) => {
     return (req, res, next) => {
-        if (!req?.roles) return res.sendStatus(401); // No roles found (not authenticated)
+        if (!req?.roles) {
+            return res.status(401).json({
+                message: "You must be authenticated to access this resource."
+            }); // No roles, Not authenticated
+        }
 
         // Check if any of the user's roles match allowedRoles
         const hasRole = req.roles.some(role => allowedRoles.includes(role.name));
 
-        if (!hasRole) return res.sendStatus(403); // Forbidden (authenticated but not allowed)
+        if (!hasRole) {
+            return res.status(403).json({
+                message: "You do not have the required permissions to access this resource."
+            }); // Authenticated but not allowed
+        }
 
         next();
     }
